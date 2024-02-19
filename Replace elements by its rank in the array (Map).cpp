c00d6e2e08d4a344a698ee53cb842f9368f682c2
Apoorvs23 <56202393+Apoorvs23@users.noreply.github.com>
2024-02-19 18:44:41 -0500

    updating last line

--- Replace elements by its rank in the array (Map).cpp
+++ Replace elements by its rank in the array (Map).cpp
@@ -2,8 +2,8 @@
 class Solution{
 public:
 
-    vector<int> replaceWithRank(vector<int> &v, int N){
-        vector<int> temp=v;
+    vector<int> replaceWithRank(vector<int> &arr, int N){
+        vector<int> temp= arr;
         sort(temp.begin(),temp.end());
         unordered_map<int,int> mp;
         int rank=1;
@@ -11,15 +11,16 @@ public:
         {
             if(mp.find(p)==mp.end())
             {
-                 mp[p]=rank;
-                 rank++;
+                mp[p]=rank;
+                rank++;
             }
         }
-       for(int i=0;i<N;i++)
-       {
-           v[i]=mp[v[i]];
-       }
-        return v;
+        for(int i=0;i<N;i++)
+        {
+             temp[i]=mp[arr[i]];
+        }
+        return temp;
+
     }
 
 };
