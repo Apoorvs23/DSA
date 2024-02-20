555f16049975daddf2277507ede7d70e133f92b6
Apoorvs23 <56202393+Apoorvs23@users.noreply.github.com>
2024-02-19 19:14:10 -0500

    shortening the code

--- Replace elements by its rank in the array (Min PQ).cpp
+++ Replace elements by its rank in the array (Min PQ).cpp
@@ -2,30 +2,25 @@
 class Solution{
 public:
 
-    vector<int> replaceWithRank(vector<int> &v, int N){
-    priority_queue< pair<int,int>, vector< pair<int,int>>, greater <pair<int,int> > > q;
-    for(int i=0;i<N;i++)
-    {
-        q.push({v[i],i});
-    }
-    int rank=0;
-    vector<int> rankVector(N);
-    int prev=-1;
-    while(!q.empty())
-    {
-        int val=q.top().first; //smalest
-        int index=q.top().second;
-        if(prev!=val)
-        {
-             rank++;
-        }
-       rankVector[index]=rank;
-        
-        q.pop();
-        prev=val;
-       
-    }
-    return rankVector;
+    vector<int> replaceWithRank(vector<int> &arr, int N){
+       priority_queue< pair<int,int>,vector<pair<int,int>> ,greater<pair<int,int>> > q;
+       vector<int> rank(N,0);
+       for(int i=0;i<N;i++)
+       {
+           q.push({arr[i],i});
+       }
+       int currentRank=0;
+       int prev=-1;
+       while(!q.empty())
+       {
+           int element=q.top().first;
+           int index=q.top().second;
+           q.pop();
+           if(prev!=element) currentRank++;
+           rank[index]=currentRank;
+           prev=element;
+       }
+       return rank;
 
     }
 
