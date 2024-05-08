2f5729be3e9871d47f67f75a92adbe3807f76518
Apoorvs23 <Apoorv.Shamma@gmail.com>
2024-05-07 22:36:37 -0400

    updating pre/suc

--- Predecessor and Successor of BST (Optimized-Inorder).cpp
+++ Predecessor and Successor of BST (Optimized-Inorder).cpp
@@ -1,31 +1,30 @@
+// This function finds predecessor and successor of key in BST.
 // It sets pre and suc as predecessor and successor respectively
 //https://www.geeksforgeeks.org/problems/predecessor-and-successor/1
-//  TC: O(N), SC: O(N)
+//  TC: O(N), SC: O(1)
 class Solution
 {
     private:
-    void inorderUtil(Node* root, vector<Node*> & vis)
+    void inorderUtil(Node* root, int key, Node* &pre, Node* &suc) // pass pointers by refernce because maybe in revursive call pointer start pointing to correct but it wont show in main functio because we are chaning the position of pointer where is point to in any function that wont be reflected inn the mainn
     {
-        if(!root) return;
-        inorderUtil(root->left,vis);
-        vis.push_back(root);
-        inorderUtil(root->right,vis);
+        if(!root || suc) return;
+        inorderUtil(root->left,key,pre,suc);
+        if(root->key<key) pre=root;
+        else if(root->key>key && !suc ) // first time
+        {
+            suc=root;
+            return;
+        }
+      //  vis.push_back(root);
+        inorderUtil(root->right,key,pre,suc);
     }
     public:
     void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
     {
         if(!root) return;
         // Your code goes here
-        vector<Node*> inOrder;
-        inorderUtil(root,inOrder); // sorted
-        for(auto node:inOrder)
-        {
-            if(node->key<key) pre=node;
-            else if(node->key>key)
-            {
-                suc=node;
-                return;
-            }
-        }
+        //vector<Node*> inOrder;
+        inorderUtil(root, key,pre, suc); // sorted
+
     }
 };
