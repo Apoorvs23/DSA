6878ddd4296dcc83e395d88cb624d2a861b51ac0
Apoorv Shamma <56202393+Apoorvs23@users.noreply.github.com>
2024-09-18 21:32:12 -0400

    Update Maximum of minimum for every window size (Brute).cpp

--- Maximum of minimum for every window size (Brute).cpp
+++ Maximum of minimum for every window size (Brute).cpp
@@ -7,26 +7,53 @@ class Solution
     //Function to find maximum of minimums of every window size.
     vector <int> maxOfMin(int arr[], int n)
     {
-        vector<int> ans;
-        for(int lengthOfTheWindow = 1; lengthOfTheWindow<=n; lengthOfTheWindow++)
+        if(n==0) return {};
+        vector<int> v;
+        for(int k=1;k<=n;k++) // size of the window
         {
-            // start + remaining Length of Window <= n-1
-            // start + (lengthOfTheWindow -1) <= n-1 -> kyonki ek toh start hi hogya
-            // start <= n-lengthOfTheWindow
-            int maxInWindowSize = INT_MIN;
-            for(int start=0; start <= n-lengthOfTheWindow; start++) 
+            int maxx = INT_MIN;
+            for(int i=0; i<=n-k ; i++) // it will decide the starting point of window 
             {
-                // now we will traverse window for each element we will find minimum
-                int minInWindow = INT_MAX;
-                for(int k = start; k < start + lengthOfTheWindow; k++) //traverse till length of the window
+                int mini = arr[i];
+                for(int j=i;j<i+k;j++)
                 {
-                    minInWindow = min(minInWindow,arr[k]);
+                    mini = min(mini,arr[j]);
                 }
-                maxInWindowSize = max(minInWindow,maxInWindowSize);
+                maxx = max(maxx,mini);
             }
-            ans.push_back(maxInWindowSize);
-            
+            v.push_back(maxx);
         }
-        return ans;
+        return v;
     }
 };
+
+
+// class Solution
+// {
+//     public:
+//     //Function to find maximum of minimums of every window size.
+//     vector <int> maxOfMin(int arr[], int n)
+//     {
+//         vector<int> ans;
+//         for(int lengthOfTheWindow = 1; lengthOfTheWindow<=n; lengthOfTheWindow++)
+//         {
+//             // start + remaining Length of Window <= n-1
+//             // start + (lengthOfTheWindow -1) <= n-1 -> kyonki ek toh start hi hogya
+//             // start <= n-lengthOfTheWindow
+//             int maxInWindowSize = INT_MIN;
+//             for(int start=0; start <= n-lengthOfTheWindow; start++) 
+//             {
+//                 // now we will traverse window for each element we will find minimum
+//                 int minInWindow = INT_MAX;
+//                 for(int k = start; k < start + lengthOfTheWindow; k++) //traverse till length of the window
+//                 {
+//                     minInWindow = min(minInWindow,arr[k]);
+//                 }
+//                 maxInWindowSize = max(minInWindow,maxInWindowSize);
+//             }
+//             ans.push_back(maxInWindowSize);
+            
+//         }
+//         return ans;
+//     }
+// };
