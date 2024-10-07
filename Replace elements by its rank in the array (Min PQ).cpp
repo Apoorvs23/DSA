b26337ab69a4e992300c056a5e1d61ae75b47a7c
Apoorv Shamma <56202393+Apoorvs23@users.noreply.github.com>
2024-10-07 09:07:53 -0400

    Update Replace elements by its rank in the array (Min PQ).cpp

--- Replace elements by its rank in the array (Min PQ).cpp
+++ Replace elements by its rank in the array (Min PQ).cpp
@@ -1,4 +1,34 @@
 //https://www.geeksforgeeks.org/problems/replace-elements-by-its-rank-in-the-array/1
+
+class Solution{
+public:
+
+    vector<int> replaceWithRank(vector<int> &arr, int n){
+    priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    
+    for(int i = 0; i< n; i++)
+    {
+        pq.push({arr[i],i});
+    }
+    int prev = -1;
+    int rank = 0;
+    vector<int> ans(n,0);
+    while(!pq.empty())
+    {
+        if(prev!=pq.top().first)
+            rank++;
+        ans[pq.top().second] = rank;
+        prev = pq.top().first;
+        pq.pop();
+        
+    }
+    return ans;
+
+    }
+
+};
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution{
 public:
 
